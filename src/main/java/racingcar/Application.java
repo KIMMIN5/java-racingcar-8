b04ee5c7dce9e5117d9296b7cd4f42c1cda3220e
KIMMIN5 <alsdh1111@naver.com>
2025-10-24 01:21:41 +0900

    feat: Main

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,15 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.Converter;
+import racingcar.controller.RaceController;
+import racingcar.view.UserView;
+
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        Race race = new Race(carNames);
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int step = Integer.parseInt(Console.readLine());
-        race.setStep(step);
-
-        System.out.println("실행 결과");
-        System.out.println("최종 우승자 : " + race.startRace());
-
+        RaceController raceController = new RaceController(new UserView(), new Converter());
+        raceController.run();
     }
 
 
