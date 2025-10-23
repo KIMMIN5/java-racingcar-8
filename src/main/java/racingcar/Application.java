596adbe6e8403d6fa48c6d73876a6e7d66d98709
KIMMIN5 <alsdh1111@naver.com>
2025-10-23 20:32:30 +0900

    feat: 자동차 경주 가능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        Race race = new Race(carNames);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int step = Integer.parseInt(Console.readLine());
+        race.setStep(step);
+
+        System.out.println("실행 결과");
+        System.out.println("최종 우승자 : " + race.startRace());
+
     }
+
+
 }
