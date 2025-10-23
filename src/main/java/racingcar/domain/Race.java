a42c221e7b895bd20f054f029eb6b8a386ca25e5
KIMMIN5 <alsdh1111@naver.com>
2025-10-24 01:16:23 +0900

    refactor: 입출력 삭제

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,51 +1,31 @@
-package racingcar;
+package racingcar.domain;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Race {
-    int step;
-    ArrayList<Car> cars;
+    private final ArrayList<Car> cars;
 
-    public Race(String carsName) {
-        this.cars = new ArrayList<Car>();
-        String[] splitCarName = carsName.split(",");
-        for(String carName: splitCarName) {
-            cars.add(new Car(carName));
-        }
+    public Race(ArrayList<Car> cars) throws IllegalArgumentException {
+        this.cars = new ArrayList<>(cars);
     }
 
-    public void setStep(int step) throws IllegalArgumentException{
-        if(step <= 0) {
-            throw new IllegalArgumentException("음수 및 0은 입력할 수 없습니다.");
-        }
-        this.step = step;
+    public ArrayList<Car> stepRace() {
+        cars.forEach(Car::forward);
+        return cars;
     }
 
-    public String startRace() {
-        for(int s=0; s<step; s++) {
-            cars.forEach(Car::forward);
-            cars.forEach(Car::status);
-            System.out.println();
-        }
-        return findWinner(cars);
+    public ArrayList<Car> getCars() {
+        return cars;
     }
 
-    public String findWinner(ArrayList<Car> cars) {
+    public ArrayList<Car> findWinner(ArrayList<Car> cars) {
         int maxDistance = cars.stream()
                 .mapToInt(Car::getDistance)
                 .max()
                 .orElse(0);
 
-        ArrayList<Car> winnerCars = cars.stream()
+        return cars.stream()
                 .filter(car -> car.getDistance() == maxDistance)
                 .collect(Collectors.toCollection(ArrayList::new));
-
-        return winnerCars.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
     }
 }
