b144a1ae867b131e2957c8d8529e89bcf889fa6a
KIMMIN5 <alsdh1111@naver.com>
2025-10-24 01:21:07 +0900

    feat: 자동차 이동 규칙

--- src/main/java/racingcar/domain/CarMoveRule.java
+++ src/main/java/racingcar/domain/CarMoveRule.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
-public interface MovingRule {
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class CarMoveRule {
     public boolean canMove() {
-        return true;
+        return Randoms.pickNumberInRange(0,9) >= 4;
     }
 }
